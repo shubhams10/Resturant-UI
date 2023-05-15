e5a0a99869c9d55cd1702632a35acbb85aefc4f0
shubhams10 <shubham8115551140@gmail.com>
2023-05-15 14:24:03 +0530

    second commit

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
 import SubHeading from './SubHeading/SubHeading';
 import Newsletter from './Footer/Newsletter';
 import FooterOverlay from './Footer/FooterOverlay';
-import MenuItem from './MenuItem/MenuItem';
+import MenuItem from './MenuItem/Menuitem';
 import Navbar from './Navbar/Navbar';
 
 export {
